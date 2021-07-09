38247123054b8f64b72bd6700b29562be0b830a9
Preeti Rawat <preetirawat711@gmail.com>
2021-07-09 17:36:59 +0530

    findNemo

--- src/academy/learnprogramming/Main.java
+++ src/academy/learnprogramming/Main.java
@@ -3,7 +3,10 @@ package academy.learnprogramming;
 public class Main {
 
     public static void main(String[] args) {
-	String[] nemo={"hello"};
+      int length=  "bksabdkbbkzxbkbadsjdj".length();
+        System.out.println(length);
+     String[] nemo={"nemo"};
+//        String[] all={"preeti", "sneha", "keerti","priya","spriya", "nemo","hello","happy","supriya"};
 	find.findNemo(nemo);
     }
 }
--- src/academy/learnprogramming/find.java
+++ src/academy/learnprogramming/find.java
@@ -2,6 +2,8 @@ package academy.learnprogramming;
 
 public class find {
     public static void findNemo(String[] arr){
+        long t0=System.currentTimeMillis();
+        System.out.println(t0);
         for(int i=0;i<arr.length;i++){
             if(arr[i]=="nemo"){
                 System.out.println("Found Nemo..!");
@@ -9,6 +11,9 @@ public class find {
             else{
                 System.out.println("Invalid String: Not Found.");
             }
+            long t1=System.currentTimeMillis();
+            System.out.println(t1);
+            System.out.println("Total execution time difference is: "+ (t1-t0) + "miliseconds");
         }
     }
 }
