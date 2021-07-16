27981193c708eb9ac8726ca6f82aa8502d34e008
Preeti Rawat <preetirawat711@gmail.com>
2021-07-16 11:20:51 +0530

    implementing_hashtable

---
+++ src/Data_Structures/hashtables/KeyValue.java
@@ -0,0 +1,19 @@
+package Data_Structures.hashtables;
+
+public class KeyValue {
+    private String key;
+    private int value;
+
+    public KeyValue(String key, int value) {
+        this.key = key;
+        this.value = value;
+    }
+
+    public String getKey() {
+        return key;
+    }
+
+    public int getValue() {
+        return value;
+    }
+}
