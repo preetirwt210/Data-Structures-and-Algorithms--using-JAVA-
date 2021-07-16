59dffcec76e0dae0f3b824fa5806985c91c681e4
Preeti Rawat <preetirawat711@gmail.com>
2021-07-16 17:38:52 +0530

    DoublyLinkedList

---
+++ src/Data_Structures/linked_lists/Node2.java
@@ -0,0 +1,13 @@
+package Data_Structures.linked_lists;
+
+public class Node2 {
+    int value;
+    Node2 next;
+    Node2 previous;
+
+    public Node2(int value) {
+        this.value = value;
+        this.next = null;
+        this.previous = null;
+    }
+}
