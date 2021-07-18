0c457cad943ee0b6c17c715071c9075133af2d38
Preeti Rawat <preetirawat711@gmail.com>
2021-07-18 18:44:07 +0530

    PriorityQueue

---
+++ src/Data_Structures/trees/Node.java
@@ -0,0 +1,11 @@
+package Data_Structures.trees;
+
+public class Node {
+    String value;
+    int priority;
+
+    public Node(String value, int priority) {
+        this.value = value;
+        this.priority = priority;
+    }
+}
